ef5779b30465747db1d393a0915b12c4b2ba2c1a
Malisa Marijan <Malisa.Marijan@rt-rk.com>
2016-11-23 08:29:06 +0100

    New file example2.py added

---
+++ example2.py
@@ -0,0 +1,26 @@
+# Example with if statements and dictionaries
+
+x=5
+if x==2:   print "x=2"
+elif x==3: print "x=3"
+else:      print "x!=2 and x!=3"
+
+
+dict1={'1': 1, '2': 2, '3': 3}
+
+if (3 is dict1['1']):
+	pass
+
+dict2={'Name': 'Pera', 'Surname': 'Petrovic', 'Age': 29}
+
+print "dictionary", dict2	 
+
+print "The first element in dictionary dict2 is", dict2['Name']
+
+print "%s is the second element of the dictionary dict2" % dict2['Surname']
+
+sn = dict2['Surname']
+print sn[0:2]
+	
+	
+### __END__ Example2
